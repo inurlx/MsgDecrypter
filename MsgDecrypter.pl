8aaf1bfec3b2bb1dc87834ce828788d6c9c6dd3b
MOHAMMED ADEL <sourceyea@gmail.com>
2020-12-14 02:48:49 +0800

    Update MsgDecrypter.pl

--- MsgDecrypter.pl
+++ MsgDecrypter.pl
@@ -4,12 +4,8 @@ use warnings;
 use Term::ANSIColor;
 use Email::Outlook::Message;
 
-print color("red"), "\n\n                         By [ MOHAMMED ADEL ] \n\n", color("reset");
-print color("white"), "              This is a sample tool that can Decrypt .msg Files . \n\n", color("reset");
-print color("blue"), "              [!] Input Examples:\n";
-print "               1- 1.msg\n               * It must be on the same directory !!";
 DoItAgain:
-print color("yellow"), "\n\n[!] Enter the File Name --> ", color("reset");
+print color("yellow"), "\n\n[!] File --> ", color("reset");
 my $filename = <STDIN>;
 if ($filename =~ /\.msg$/i)
 {
@@ -21,7 +17,7 @@ if ($filename =~ /\.msg$/i)
     open(FILE, ">>DESCRIPTED_MSG.txt");
     print FILE $mime->as_string;
     close (FILE);
-    print color("white"), "\n\nDecrypted Information Can Be Found At [DESCRIPTED_MSG.txt] \n\n", color("reset");
+    print color("white"), "\n\nResults At [DESCRIPTED_MSG.txt] \n\n", color("reset");
 
 }
 else
